c6ff8a1ae3282b5bcd54a6baa121100824ddc518
Shin Kuboaki <kuboaki@gmail.com>
2021-05-22 04:25:29 +0900

    問題を作成した

--- codes/main.rb
+++ codes/main.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-require_relative `app`
+require_relative 'app'
 
-
-if $PROGRAM_NAME == __FILE__
-  abort "usage: ruby #{$PROGRAM_NAME} db_file" if ARGV.empty?
-  mybook = MyBook.new(ARGV[0])
-  mybook.run
-end
+abort "usage: ruby #{$PROGRAM_NAME} db_file" if ARGV.empty?
+addressbook = AddressBook.new(ARGV[0])
+addressbook.run
